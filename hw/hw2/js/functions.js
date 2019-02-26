1a234630a77b33a919c8280b7e9bc5c1555af87f
Angelo Calitis <acalitis@csumb.edu>
2019-02-26 22:34:32 +0000

    Homework 2

--- hw/hw2/js/functions.js
+++ hw/hw2/js/functions.js
@@ -14,7 +14,7 @@ var q3_answer = "1";
 var q4_answer = "75";
 var q5_answer = "logarithmically";
 
-var miko_expressions = ["cheery", "understanding", "displeased"];
+var miko_expressions = ["cheery", "understanding", "concerned", "angry"];
 
 var miko_reactions = ["", "", "", "", ""];
 var miko_text = ["", "", "", "", ""];
@@ -25,30 +25,29 @@ var miko_text = ["", "", "", "", ""];
 Function to set appropriate image based on answers for questions 1-4
 If statement handles questions 1-3
 Else statement handles question 4
+2nd If/else handles question 5
 */ 
 function setMiko(question_num, question) {
     let val = question.val();
-    if (val <= 4) {
+    //Change else if statement structure to make question 5 work
+    
+    if (val === "linearly") {
+        miko_reactions[question_num] = miko_expressions["angry"];  
+    } else if (val === "logarithmically") {
+        miko_reactions[question_num] = miko_expressions["cheery"];  
+    } else if (val <= 4) {
         miko_reactions[question_num] = miko_expressions[val-1];  
     } else {
-        if (val >= 90) {
+        if (val >= 100) {
+            miko_reactions[question_num] = miko_expressions[3]; 
+        } else if (val < 100 & val >= 90) {
             miko_reactions[question_num] = miko_expressions[2]; 
         } else if (val < 90 && val >= 76) {
             miko_reactions[question_num] = miko_expressions[1]; 
         } else {
             miko_reactions[question_num] = miko_expressions[0]; 
         }
-        
     }
-}
-
-function isFormValid() {
-    let isValid = true;
-    if ($("#q1").val() == "") {
-        isValid = false;
-        $("#validSubmission").html("Question 1 not answered");
-    }
-    return isValid;
 }
 
 
@@ -59,9 +58,36 @@ $("#submitButton").on( "click", function() {
     let q4_response = $("#q4").val();
     let q5_response = $("#q5").val();
     
+    function isFormValid() {
+        let isValid = true;
+        if (q1_response === undefined) {
+            isValid = false;
+            $("#q1text").css("color", "red");
+        }
+        if (q2_response === undefined) {
+            isValid = false;
+            $("#q2text").css("color", "red");
+        }
+        if (q3_response === undefined) {
+            isValid = false;
+            $("#q3text").css("color", "red");
+        }
+        if (q4_response == "") {
+            isValid = false;
+            $("#q4text").css("color", "red");
+        }
+        if (q5_response == "") {
+            isValid = false;
+            $("#q5text").css("color", "red");
+        }
+        return isValid;
+    }
+    
     if (!isFormValid()) {
         return;
     }
+    
+    $("#quiz").css("display", "hide");
     
     //Question 1
     setMiko(0, q1);
@@ -89,17 +115,17 @@ $("#submitButton").on( "click", function() {
             "Overhearing chatter with your music is temporary. Noise-induced hearing loss is irreversible.";
         } else {
             miko_reactions[1] = miko_expressions[0];
-            miko_text[0] = "You think I would rag on you for not hearing your surroundings with noise-cancelling headphones on?" +
+            miko_text[1] = "You think I would rag on you for not hearing your surroundings with noise-cancelling headphones on?" +
             "That'd make me a hypocrite!";
         }
     } else if (q2_response === 2) {
-        miko_text[0] = "You have good listening habits if you can still hear your surroundings.\n Or you use earbuds.\n " +
+        miko_text[1] = "You have good listening habits if you can still hear your surroundings.\n Or you use earbuds.\n " +
         "Either way, caution should be taken. 'Hearing surroundings' is not equivalent to 'safe listening levels'." + 
         "Ultimately, 'safe levels' varies based on what music you listen to, how loud your surroundings actually are, " +
         "and what listening peripheral you're equipped with. Play some music in a quiet room. Hold your earphones an arm's length away." +
         "If you can hear the music, your typical listening level is unhealthy for your ears!";
     } else {
-        miko_text[0] = "Being able to hear a person talking to you while using earphones is, frankly, pretty annoying, but ultimately " + 
+        miko_text[1] = "Being able to hear a person talking to you while using earphones is, frankly, pretty annoying, but ultimately " + 
         "healthy for your hearing. If you ever feel tempted to bump up the music, keep the elevated levels for only a song or two, or " +
         "even relocating yourself to a quieter haven.";
     }
@@ -107,37 +133,52 @@ $("#submitButton").on( "click", function() {
     //Question 3
     setMiko(2, q3);
     if (q3_response > 2) { //Longer than an hour
-        if (q2_response === 3) { //"block out your surroundings"
-            if (q1_response > 1) { //Regular earbuds or headphones
-                miko_text[1] = "Headphones or earbuds, you should never play music loud enough to block out your surroundings." +
-                "One tip to check if your volume is healthy or not is to hold your headphones/earbuds an arm's length away" +
-                "in a quiet room. If you can hear the music, the volume is too loud!\n " +
-                "Overhearing chatter with your music is temporary. Noise-induced hearing loss is irreversible.";
-            } else { //Noise cancelling
-                miko_reactions[1] = miko_expressions[0];
-                miko_text[0] = "You think I would rag on you for not hearing your surroundings with noise-cancelling headphones on?" +
-                "That'd make me a hypocrite!";
-            }
+        miko_text[2] = "Listening for longer than an hour at a time isn't exactly recommended, even if you're listening at a safe volume.\n" +
+        "Prolonged exposure to music through audio peripherals can increase stress levels; being a busybody person, more stress is the last thing " +
+        "anyone would want. Give your ears a short rest before you jam on the tunes.";
     } else { // Less than an hour
-        miko_text[0] = "Being able to hear a person talking to you while using earphones is, frankly, pretty annoying, but ultimately " + 
-        "healthy for your hearing. If you ever feel tempted to bump up the music, keep the elevated levels for only a song or two, or " +
-        "even relocating yourself to a quieter haven.";;
+        miko_text[2] = "You really know how to look after your health in multiple ways if you give your ears a break so much. Keep at it!";
+    }
+
+    //Question 4
+    setMiko(3, q4);
+    if (q4_response >= 100) {
+        miko_text[3] = "At more than 100 dB, you only have LESS THAN 15 minutes of safe listening before damage sets in!";
+    } else if (q4_response < 100 & q4_response >= 90) {
+        miko_text[3] = "Between 90 and 100 dB, safe listening ranges from 2 hours to 15 minutes respectively. Be sure to keep track of time, and don't do it repeatedly!";
+    } else if (q4_response < 90 && q4_response >= 76) {
+        miko_text[3] = "Between 75 and 90 dB, you can go for over an hour to about 2 hours of continuous listening. It's not recommended; please take breaks and enjoy the quite more!"; 
+    } else {
+        miko_text[3] = "At this level, you can technically continue listening to music forever with no damage to your ears. But still, take breaks!";
+    }
+
+    //Question 5
+    setMiko(4, q5);
+    if (q5_response === "linearly") { //Longer than an hour
+        miko_text[2] = "Nope. Safe listening decreases logarithmically as the sound level increases. At 100 dB, the safe listening is about 15 minutes. At 110 dB, the listening " +
+        "decreases to 30 seconds!";
+    } else { // Less than an hour
+        miko_text[2] = "You're right! Safe listening decreases logarithmically as the sound level increases. Good job.";
     }
     
-    //End game
-    $("#total").html("The total Score is: " + total_points);
-    totalTimes++;
-    localStorage.setItem("totalTimesTaken",totalTimes);
-    $("#totalTimes").html("Quiz taken: " + totalTimes + " time(s)");
-    scoresArray.push(total_points);
-    $("#prevScores").html("Score History: ");
-    scoresArray.forEach(function(score){
-        $("#prevScores").append(score + " ");
-    });
+    $("#response1").text(miko_text[0]);
+    $("#img1").attr("src","img/" + miko_reactions[0]);
+
+    $("#response2").text(miko_text[1]);
+    $("#img2").attr("src","img/" + miko_reactions[1]);
     
-    localStorage.setItem("scoreHistory",scoresArray);
+    $("#response3").text(miko_text[2]);
+    $("#img3").attr("src","img/" + miko_reactions[2]);
+    
+    $("#response4").text(miko_text[3]);
+    $("#img4").attr("src","img/" + miko_reactions[3]);
 
-
+    $("#response5").text(miko_text[4]);
+    $("#img5").attr("src","img/" + miko_reactions[4]);
+    
+    $("#responses").css("display", "inline");
+    
+    //End game
 });
 
 // guessSubmit.addEventListener('click', checkGuess);
---
+++ practice/p4/js/functions.js
@@ -0,0 +1,3 @@
+$("#unit1").change(function(){
+  $("#unit1price").text($ + $('#tr1 td').eq(1).text())
+}); 
